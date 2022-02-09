41db16241e7071b580339c06d325cabe2acb41db
benoit sommariva <benoitsommariva@gmail.com>
2022-02-09 16:11:25 +0100

    destroy review

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     resources :restaurants do
-      resources :reviews
+      resources :reviews, only: [:new, :create, :index]
   end
-
+    resources :reviews, only: [ :show, :edit, :update, :destroy ]
 end
